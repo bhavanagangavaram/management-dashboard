25be04f835aa3ca81b51092c7ea3f1b6486a360e
gangavaram bhavana <bhavanagangavaram1@gmail.com>
2026-06-29 16:03:29 +0530

    Add gh-pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/management-dashboard/",
   plugins: [react()],
   test: {
     globals: true,
